50b0590549b339f953f7871491c6f283984558af
Shriraj Dash <Shriraj.dash2016@gmail.com>
2021-02-18 08:33:41 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Toolbar, ToolbarBackAction, ToolbarContent, ToolbarAction } from 'react-native-paper';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class MyComponent extends React.Component {
+  render() {
+    return (
+        <Toolbar>
+          <ToolbarBackAction
+              onPress={this._goBack}
+          />
+          <ToolbarContent
+              title="Title"
+              subtitle="Subtitle"
+          />
+          <ToolbarAction icon="search" onPress={this._onSearch} />
+          <ToolbarAction icon="more-vert" onPress={this._onMore} />
+        </Toolbar>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
